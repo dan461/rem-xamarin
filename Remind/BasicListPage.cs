6e7f025f2bae63a9ab2229f5050cdfb5fc617ccc
Dan Shriver <dshriver53@gmail.com>
2018-10-15 19:27:50 -0500

    * Remind.sln: * App.xaml: * Main.cs: * Remind.csproj: * MainPage.xaml: * Info.plist: * Tests.cs: * MainPage.xaml.cs: * BasicListPage.cs: * AppDelegate.cs: * Remind.iOS.csproj: * Entitlements.plist: * MainActivity.cs: * Default.png: * AppInitializer.cs: * Default@2x.png: * Remind.Android.csproj: * Remind.UITests.csproj: * AboutAssets.txt: * AssemblyInfo.cs: * Default-568h@2x.png: * Default-Portrait.png: * AssemblyInfo.cs: * colors.xml: * styles.xml: * Tabbar.axml: * AboutResources.txt: * LaunchScreen.storyboard: * Default-Portrait@2x.png: * Toolbar.axml: * AndroidManifest.xml: * Icon.png: * icon.png: * Resource.designer.cs: * Icon.png: * Icon.png: * Icon.png: * icon.xml: * Icon80.png: * Icon76.png: * Icon58.png: * Icon29.png: * Icon87.png: * Icon20.png: * Icon60.png: * Icon40.png: * Icon152.png: * Icon167.png: * Icon180.png: * icon_round.xml: * Icon120.png: * Icon1024.png: * Contents.json: * launcher_foreground.png: * launcher_foreground.png: * launcher_foreground.png: * launcher_foreground.png: * launcher_foreground.png:
    * App.xaml.cs: Replaced default main page with tabbed page with a list
      view
    
    * .gitignore: Added Git repo URL. Replaced main page with a tab page
      with a list view as a child.

---
+++ Remind/BasicListPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+
+namespace Remind
+{
+    public class BasicListPage : ContentPage
+    {
+        public BasicListPage()
+        {
+            var listView = new ListView();
+            listView.ItemsSource = new[] { "a", "b", "c" };
+
+            // using ItemTapped
+            listView.ItemTapped += async (sender, e) => {
+                await DisplayAlert("Tapped", e.Item + " row was tapped", "OK");
+                Debug.WriteLine("Tapped: " + e.Item);
+                ((ListView)sender).SelectedItem = null; // de-select row
+            };
+
+            // if using ItemSelected
+            listView.ItemSelected += (sender, e) =>
+            {
+                if (e.SelectedItem == null) return;
+                Debug.WriteLine("Selected: " + e.SelectedItem);
+                ((ListView)sender).SelectedItem = null; // deselect row
+            };
+
+            Padding = new Thickness(0, 20, 0, 0);
+            Content = listView;
+        }
+    }
+}
