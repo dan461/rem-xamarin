3cfe1a6c9fed86c7ab3d553f0a44a000c0e7db15
Dan Shriver <dshriver53@gmail.com>
2018-10-15 19:57:32 -0500

    * BasicListXaml.xaml.cs:
    * BasicListXaml.xaml: Updated with onItemSelected

---
+++ Remind/BasicListXaml.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace Remind
+{
+    public partial class BasicListXaml : ContentPage
+    {
+        public BasicListXaml()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new[] { "a", "b", "c" };
+        }
+
+        void OnItemTapped(object sender, ItemTappedEventArgs e) {
+            if (e == null) return; // has been set to null, dont process tap event
+            Debug.WriteLine("Tapped: " + e.Item);
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
