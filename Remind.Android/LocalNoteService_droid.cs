5d3ac61c8d5f59c0586a5a11f5655151a5c79cc6
Dan Shriver <dshriver53@gmail.com>
2018-10-19 15:21:59 -0500

    * LocalNoteService_droid.cs: Using BroadcastReciever and   AlarmService for very basic scheduling of notification.
    * Remind.Android.csproj:

--- Remind.Android/LocalNoteService_droid.cs
+++ Remind.Android/LocalNoteService_droid.cs
@@ -3,7 +3,10 @@ using Remind.Droid;
 using Xamarin.Forms;
 using Android.Support.V4.App;
 using Android.Content;
+using Android.App;
+using Java.Util;
 
+// Implements LocalNoteService DependencyService to schedule and publish Local Notifications. 
 
 
 [assembly: Dependency(typeof(LocalNoteService_droid))]
@@ -11,11 +14,15 @@ using Android.Content;
 
 namespace Remind.Droid
 {
-    public class LocalNoteService_droid : LocalNoteService
+    [BroadcastReceiver]
+    public class LocalNoteService_droid : BroadcastReceiver, LocalNoteService
     {
         static readonly int NOTIFICATION_ID = 1000;
         static readonly string CHANNEL_ID = "remind_location_notification";
         internal static readonly string COUNT_KEY = "count";
+
+        private string NoteTitle = "";
+        private string NoteMessage = "";
 
         public void TestNote(string title, string message)
         {
@@ -23,18 +30,38 @@ namespace Remind.Droid
 
             Context context = Android.App.Application.Context;
 
+            NoteTitle = title;
+            NoteMessage = message;
+
+            var noteIntent = new Intent(context, typeof(LocalNoteService_droid));
+            var pendingIntent = PendingIntent.GetBroadcast(context, 0, noteIntent, 0);
+
+            var now = DateTime.Now;
+            var showTime = now.AddSeconds(30.0);
+
+            //var calendar = Calendar.Instance;
+            //calendar.Set(CalendarField.Mi)
+            AlarmManager alarmManager = (AlarmManager)context.GetSystemService(Context.AlarmService);
+            alarmManager.SetExact(AlarmType.ElapsedRealtimeWakeup, showTime.Millisecond, pendingIntent);
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            // Create the notification here so that it is not added to the Intent, avoiding Intent size limit issues
+            CreateAndPublishNotification(context);
+        }
+
+        private void CreateAndPublishNotification(Context context)
+        {
             var builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                                                 .SetSmallIcon(Resource.Drawable.icon)
-                                                .SetContentTitle(title)
-                                                .SetContentText(message)
-                                                .SetWhen(30);
+                                                .SetContentTitle(NoteTitle)
+                                                .SetContentText(NoteMessage);
 
 
             var noteManager = NotificationManagerCompat.From(context);
             noteManager.Notify(NOTIFICATION_ID, builder.Build());
-
-
-
         }
+
     }
 }
